8f23e04fec8fdfa2c6e803b7954e101fcb2e2920
brian <brian22e3@hotmail.com>
2018-02-10 19:42:16 -0800

    Adding two communicator Test Case

--- nachos/threads/Communicator.java
+++ nachos/threads/Communicator.java
@@ -15,7 +15,7 @@ public class Communicator {
 	/**
 	 * Allocate a new communicator.
 	 */
-	private Lock lock;
+	private Lock lock=new Lock();
 	private static int speakerNum = 0;
 	private static int listenerNum = 0;
 	private int word = 0;
@@ -74,12 +74,56 @@ public class Communicator {
 		}
 		lock.release();		
 		Machine.interrupt().restore(intStatus);
+		System.out.println(KThread.currentThread().getName()+" get message");
 		return speakerQueue.poll();
 	}
 	
 	//self test
 	public static void selfTest() {
 		
+		class listenerThread implements Runnable{
+			Communicator c;
+			public listenerThread(Communicator c){
+				this.c=c;
+			}
+			@Override
+			public void run() {
+				System.out.println(KThread.currentThread().getName()+" is listening");
+				c.listen();
+			}
+			
+		}
+		class speakerThread implements Runnable{
+			Communicator c;
+			int word;
+			public speakerThread(Communicator c, int word){
+				this.c=c;
+				this.word=word;
+			}
+			@Override
+			public void run() {
+				System.out.println(KThread.currentThread().getName()+" said "+word);
+				c.speak(word);
+			}
+		}
+		
+		//VAR1: Test for one speaker, one listener, speaker waits for listener
+		Communicator c1=new Communicator();
+		Alarm a1=new Alarm();
+		KThread s1=new KThread(new speakerThread(c1,1)).setName("s1");
+		s1.fork();
+		a1.waitUntil(5000);
+		KThread l1=new KThread(new listenerThread(c1)).setName("l1");
+		l1.fork();
+		l1.join();
+		//VAR2: Test for one speaker, one listener, listener waits for speaker
+		Communicator c2=new Communicator();
+		KThread l2=new KThread(new listenerThread(c2)).setName("l2");
+		l2.fork();
+		a1.waitUntil(5000);
+		KThread s2=new KThread(new speakerThread(c2,2)).setName("s2");
+		s2.fork();
+		s2.join();
 	}
 		
 }
